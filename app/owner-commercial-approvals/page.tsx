ae9229ba8e062c8ee8e7c94d4baf3f16cda3498f
kajeevan1992 <kajeevan1992@icloud.com>
2026-04-15 18:13:48 +0100

    v108

---
+++ app/owner-commercial-approvals/page.tsx
@@ -0,0 +1,5 @@
+import { OwnerCommercialApprovalsPage } from '@/modules/super-admin/pages/owner-commercial-approvals-page';
+
+export default function Page() {
+  return <OwnerCommercialApprovalsPage />;
+}

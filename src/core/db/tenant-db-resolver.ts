e496b6c3fa35c6585e2441016044e7dad7291f1f
kajeevan1992 <kajeevan1992@icloud.com>
2026-04-24 13:27:30 +0100

    v178

---
+++ src/core/db/tenant-db-resolver.ts
@@ -0,0 +1,7 @@
+// v178 Tenant DB Resolver foundation
+export async function resolveTenantDb() {
+  return {
+    ok: false,
+    message: "Tenant DB wiring placeholder (next build will fully connect Prisma/pg)"
+  };
+}

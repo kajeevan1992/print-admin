91b11aec187064536168af78aefd4a8365353246
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-22 19:16:54 +0100

    Build 13: add email outbox admin route

---
+++ app/email-outbox/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { EmailOutboxPage } from '@/modules/email/pages/email-outbox-page';
+
+export default function Page() {
+  return <EmailOutboxPage />;
+}

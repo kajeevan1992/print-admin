8010efbbc4021f2c5e31c75f7c17d021cc6dd92a
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-22 20:02:52 +0100

    Build 15: add email settings admin route

---
+++ app/email-settings/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { EmailSettingsPage } from '@/modules/email/pages/email-settings-page';
+
+export default function Page() {
+  return <EmailSettingsPage />;
+}

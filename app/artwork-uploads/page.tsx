71d562c02db3fcafaea24eb947742dadf6a8bace
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-22 17:47:28 +0100

    Build 9.1: add artwork uploads admin route

---
+++ app/artwork-uploads/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { ArtworkUploadsPage } from '@/modules/artwork/pages/artwork-uploads-page';
+
+export default function Page() {
+  return <ArtworkUploadsPage />;
+}

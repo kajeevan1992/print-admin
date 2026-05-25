756061e7723a1c63dfdcf2eb645bd37a372bf5b4
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-25 13:18:32 +0100

    Build 32: add settings invoice page route

---
+++ app/settings/invoice/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { InvoiceSettingsPage } from '@/modules/settings/pages/invoice-settings-page';
+
+export default function Page() {
+  return <InvoiceSettingsPage />;
+}

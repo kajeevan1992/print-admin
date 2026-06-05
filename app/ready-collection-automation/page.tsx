498da6748c4e3be464b829e498da025013c9ba34
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 23:24:13 +0100

    Build 64: add ready collection automation route

---
+++ app/ready-collection-automation/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { ReadyCollectionAutomationPage } from '@/modules/collection/pages/ready-collection-automation-page';
+
+export default function Page() {
+  return <ReadyCollectionAutomationPage />;
+}

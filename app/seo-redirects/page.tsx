c4fcb512d9f09defd0090b1214a46fecd0725b6a
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 23:32:16 +0100

    Add SEO redirects admin route

---
+++ app/seo-redirects/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { SeoRedirectManagerPage } from '@/modules/seo/pages/seo-redirect-manager-page';
+
+export default function Page() {
+  return <SeoRedirectManagerPage />;
+}

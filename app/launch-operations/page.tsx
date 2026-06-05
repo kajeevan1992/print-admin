0a4f5602f8073b5a7e27183aaeb45beec94ab3a6
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 23:37:22 +0100

    Build 65: add launch operations route

---
+++ app/launch-operations/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchOperationsPage } from '@/modules/launch/pages/launch-operations-page';
+
+export default function Page() {
+  return <LaunchOperationsPage />;
+}

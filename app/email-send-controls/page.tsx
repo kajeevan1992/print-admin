fbb5a21ce9a9c305229240a23cff9c4f2351161d
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 23:04:30 +0100

    Build 63: add email send controls route

---
+++ app/email-send-controls/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { EmailSendControlsPage } from '@/modules/email/pages/email-send-controls-page';
+
+export default function Page() {
+  return <EmailSendControlsPage />;
+}

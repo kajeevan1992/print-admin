34fff7712d4922b761c6573575c07573ddffb061
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 23:40:59 +0100

    Add local SEO generator admin route

---
+++ app/seo-local-generator/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LocalSeoGeneratorPage } from '@/modules/seo/pages/local-seo-generator-page';
+
+export default function Page() {
+  return <LocalSeoGeneratorPage />;
+}

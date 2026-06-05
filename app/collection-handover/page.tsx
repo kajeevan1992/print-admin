da47b8b3f1a19d15bcb57f7a0d5bd92f5abfef5f
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 22:26:15 +0100

    Build 61: add collection handover route

---
+++ app/collection-handover/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { CollectionHandoverPage } from '@/modules/collection/pages/collection-handover-page';
+
+export default function Page() {
+  return <CollectionHandoverPage />;
+}

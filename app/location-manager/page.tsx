d48c170e077577865d58db17755484708cb1d829
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 20:50:42 +0100

    Build 57: add location manager route

---
+++ app/location-manager/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LocationManagerPage } from '@/modules/locations/pages/location-manager-page';
+
+export default function Page() {
+  return <LocationManagerPage />;
+}

9fb57816b9873df2741fb9996845159c123af1e2
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-06 12:41:28 +0100

    Build 67: add launch test order route

---
+++ app/launch-test-order/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchTestOrderPage } from '@/modules/launch/pages/launch-test-order-page';
+
+export default function Page() {
+  return <LaunchTestOrderPage />;
+}

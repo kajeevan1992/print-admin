e1b521044a163f1316ffcbb2f33d6e591727f10f
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-06 14:39:49 +0100

    Build 68: add launch test data cleanup route

---
+++ app/launch-test-data-cleanup/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchTestDataCleanupPage } from '@/modules/launch/pages/launch-test-data-cleanup-page';
+
+export default function Page() {
+  return <LaunchTestDataCleanupPage />;
+}

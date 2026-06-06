4a311894197e2bc8654c7337d30d98f210e5e915
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-06 12:17:36 +0100

    Build 66: add launch readiness route

---
+++ app/launch-readiness/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchReadinessRunnerPage } from '@/modules/launch/pages/launch-readiness-runner-page';
+
+export default function Page() {
+  return <LaunchReadinessRunnerPage />;
+}

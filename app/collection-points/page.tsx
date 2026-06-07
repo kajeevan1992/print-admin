1a96c3f156dae7cae9d1ac1a746173e036f44667
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-07 17:39:18 +0100

    Add local collection points page route

---
+++ app/collection-points/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { CollectionPointsPage } from '@/modules/locations/collection-points-page';
+
+export default function Page() {
+  return <CollectionPointsPage />;
+}

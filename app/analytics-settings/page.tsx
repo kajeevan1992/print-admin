709ebe8a8ad7f0fb0b72c940ee215084dc59b83e
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-08 22:05:11 +0100

    Add analytics settings alias route

---
+++ app/analytics-settings/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { TrackingSettingsPage } from '@/modules/analytics/tracking-settings-page';
+
+export default function Page() {
+  return <TrackingSettingsPage />;
+}

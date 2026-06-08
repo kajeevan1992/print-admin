4e300db12daa0d0f29a24568f7b623f21ed0575e
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-08 21:54:11 +0100

    Add Google Search Console admin route

---
+++ app/seo-search-console/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { GoogleSearchConsolePage } from '@/modules/seo/pages/google-search-console-page';
+
+export default function Page() {
+  return <GoogleSearchConsolePage />;
+}

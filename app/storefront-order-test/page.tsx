305a5190e2f81decf5c4fab946ff733c394bd487
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-09 22:13:00 +0100

    Add storefront order E2E test admin route

---
+++ app/storefront-order-test/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StorefrontOrderTestPage } from '@/modules/launch/storefront-order-test-page';
+
+export default function Page() {
+  return <StorefrontOrderTestPage />;
+}

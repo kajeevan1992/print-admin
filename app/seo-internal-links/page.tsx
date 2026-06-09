9a61846eca7c0294d8b09a6d663fcc7de5630f61
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-09 18:11:58 +0100

    Add SEO internal linking admin route

---
+++ app/seo-internal-links/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { SeoInternalLinkingPage } from '@/modules/seo/pages/seo-internal-linking-page';
+
+export default function Page() {
+  return <SeoInternalLinkingPage />;
+}

cfd444880c556738f4dd35a8ca9378c53c37fc22
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-09 23:23:53 +0100

    Add payment checkout QA admin route

---
+++ app/payment-checkout-qa/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PaymentCheckoutQaPage } from '@/modules/launch/payment-checkout-qa-page';
+
+export default function Page() {
+  return <PaymentCheckoutQaPage />;
+}

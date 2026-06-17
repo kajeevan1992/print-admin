56ef716d7e7180d65a45069ede4eab4d94d2cf4b
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-18 00:43:43 +0100

    Add admin menu builder route

---
+++ app/menu-builder/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StorefrontMenuBuilderPage } from '@/modules/content/pages/storefront-menu-builder-page';
+
+export default function Page() {
+  return <StorefrontMenuBuilderPage />;
+}

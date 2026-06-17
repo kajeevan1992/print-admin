1f81db1e10350999be3efe817d39ae7f7acbb71b
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-18 00:12:39 +0100

    Add admin launch security route

---
+++ app/admin-launch-security/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { AdminLaunchSecurityPage } from '@/modules/launch/admin-launch-security-page';
+
+export default function Page() {
+  return <AdminLaunchSecurityPage />;
+}

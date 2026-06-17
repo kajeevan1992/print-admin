ce694922a0da43f81bdfa85ddb0731a6902b0cc9
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-18 00:21:47 +0100

    Add data continuity route

---
+++ app/data-continuity/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { DataContinuityPage } from '@/modules/launch/data-continuity-page';
+
+export default function Page() {
+  return <DataContinuityPage />;
+}

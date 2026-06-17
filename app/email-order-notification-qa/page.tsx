c3a27af1b3d8c8bf395556f44b9019f807e64e01
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-17 23:50:17 +0100

    Add email order notification QA route

---
+++ app/email-order-notification-qa/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { EmailOrderNotificationQaPage } from '@/modules/launch/email-order-notification-qa-page';
+
+export default function Page() {
+  return <EmailOrderNotificationQaPage />;
+}

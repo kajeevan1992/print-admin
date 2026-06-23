07ff577a8aa1816620046c29b07dc56330157939
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-23 17:01:22 +0100

    Add OAuth status route

---
+++ app/oauth/page.tsx
@@ -0,0 +1,7 @@
+import { GoogleOAuthPage } from '@/modules/platform/google-oauth-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <GoogleOAuthPage />;
+}

4ebfa3479c250715fffb0e6e8a6c22366666cf2f
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-23 16:47:53 +0100

    Add security events page route

---
+++ app/security-events/page.tsx
@@ -0,0 +1,7 @@
+import { SecurityEventsPage } from '@/modules/platform/security-events-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <SecurityEventsPage />;
+}

02f6d852c4224d537d7beb4f0477429500667b92
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-23 16:32:19 +0100

    Add DB backed credentials route

---
+++ app/credentials/page.tsx
@@ -0,0 +1,7 @@
+import { CredentialsPage } from '@/modules/platform/credentials-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <CredentialsPage />;
+}

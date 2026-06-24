b666533f0f65c20a936d7f02f05c6ae5e9064ab1
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-24 22:33:55 +0100

    Add payment accounts route

---
+++ app/payment-accounts/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PaymentAccountsPage } from '@/modules/settings/pages/payment-accounts-page';
+
+export default function Page() {
+  return <PaymentAccountsPage />;
+}

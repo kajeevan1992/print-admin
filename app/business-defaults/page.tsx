afbbfb513d882cad05be36acea36052270b75c2b
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-24 22:20:01 +0100

    Add business defaults page

---
+++ app/business-defaults/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { DefaultStoreBootstrapPage } from '@/modules/platform/default-store-bootstrap-page';
+
+export default function Page() {
+  return <DefaultStoreBootstrapPage />;
+}

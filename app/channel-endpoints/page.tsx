2a5ee4a64e72a2ac45f0c9fbf8ac833ed23f4f69
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-24 23:06:00 +0100

    Add channel endpoints route

---
+++ app/channel-endpoints/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { ChannelEndpointsPage } from '@/modules/channels/pages/channel-endpoints-page';
+
+export default function Page() {
+  return <ChannelEndpointsPage />;
+}

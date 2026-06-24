892f02fdfc518d2c4ab5d7ea21b8634d4e11a735
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-24 23:14:57 +0100

    Add live flow check route

---
+++ app/live-flow-check/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StorefrontLiveFlowPage } from '@/modules/launch/storefront-live-flow-page';
+
+export default function Page() {
+  return <StorefrontLiveFlowPage />;
+}

a4a36064d34b9bbd64336360826d38e5b8ea633a
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 00:12:14 +0100

    Add site designer route

---
+++ app/site-designer/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { HostedThemeEditorPage } from '@/modules/themes/pages/hosted-theme-editor-page';
+
+export default function Page() {
+  return <HostedThemeEditorPage />;
+}

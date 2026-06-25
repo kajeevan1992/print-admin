8bb51d36fbe6eb5107b7372c2d4226ab86999a40
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 18:39:23 +0100

    Add theme versions route

---
+++ app/theme-versions/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { ThemeVersionManagerPage } from '@/modules/themes/pages/theme-version-manager-page';
+
+export default function Page() {
+  return <ThemeVersionManagerPage />;
+}

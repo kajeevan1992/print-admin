744e9426c8e25967282cc5404bd55379e182db38
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 18:38:21 +0100

    Add design bundles route

---
+++ app/design-bundles/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { DesignBundleManagerPage } from '@/modules/themes/pages/design-bundle-manager-page';
+
+export default function Page() {
+  return <DesignBundleManagerPage />;
+}

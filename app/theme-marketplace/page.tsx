cf28e628302f465d7fd9237288fda7acf9b9b116
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 18:42:06 +0100

    Add theme marketplace route

---
+++ app/theme-marketplace/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { ThemeMarketplacePage } from '@/modules/themes/pages/theme-marketplace-page';
+
+export default function Page() {
+  return <ThemeMarketplacePage />;
+}

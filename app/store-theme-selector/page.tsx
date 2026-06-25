496df807fab955d9272f89c6eda8d1facf9e1108
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 17:32:03 +0100

    Add tenant store theme selector route

---
+++ app/store-theme-selector/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StoreThemeSelectorPage } from '@/modules/themes/pages/store-theme-selector-page';
+
+export default function Page() {
+  return <StoreThemeSelectorPage />;
+}

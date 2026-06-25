96c7ea8b558a2f84f5a049322697baa7f6b6183b
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 17:31:05 +0100

    Add platform theme library route

---
+++ app/theme-library/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PlatformThemeLibraryPage } from '@/modules/themes/pages/platform-theme-library-page';
+
+export default function Page() {
+  return <PlatformThemeLibraryPage />;
+}

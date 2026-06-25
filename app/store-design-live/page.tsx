76c2958ef6986e0c8ca23676b540c699018fca7d
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 18:41:29 +0100

    Add store design go live route

---
+++ app/store-design-live/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StoreDesignGoLivePage } from '@/modules/themes/pages/store-design-go-live-page';
+
+export default function Page() {
+  return <StoreDesignGoLivePage />;
+}

ebea234afca91aadbef8715d1e2deecf9681b6b9
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 22:47:18 +0100

    Add store domains route

---
+++ app/store-domains/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StoreDomainsPage } from '@/modules/channels/pages/store-domains-page';
+
+export default function Page() {
+  return <StoreDomainsPage />;
+}

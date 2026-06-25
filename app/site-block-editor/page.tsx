d9ebd47f1054938b70973ec39896eea9e692f2ca
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 17:33:07 +0100

    Add site block editor route

---
+++ app/site-block-editor/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PuckThemeEditorPage } from '@/modules/themes/pages/puck-theme-editor-page';
+
+export default function Page() {
+  return <PuckThemeEditorPage />;
+}

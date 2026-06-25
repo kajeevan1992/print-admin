1d15d89702681280b7aa0716958638fda5aa3c11
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-25 23:12:49 +0100

    Add store allowances route

---
+++ app/store-allowances/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StoreAllowancePage } from '@/modules/platform/pages/store-allowance-page';
+
+export default function Page() {
+  return <StoreAllowancePage />;
+}

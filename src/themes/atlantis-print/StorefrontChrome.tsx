f5682dc784ae2e1dce004bb7a3fdb43f0bba47ca
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-30 14:45:57 +0100

    Remove broken Atlantis native UI wrapper

--- src/themes/atlantis-print/StorefrontChrome.tsx
+++
@@ -1,4 +0,0 @@
-import AtlantisHeader from './AtlantisHeader';
-import AtlantisFooter from './AtlantisFooter';
-
-export default function StorefrontChrome({currentPath='/', navItems=[], storeBase='/', children}: any){return <div><AtlantisHeader currentPath={currentPath} navItems={navItems} storeBase={storeBase} />{children}<AtlantisFooter storeBase={storeBase} /></div>;}

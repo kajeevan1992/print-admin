3240ff3773d86be2794bbf58f1a69f5aefc717ac
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-30 13:30:43 +0100

    Expand Atlantis brand constants

--- src/themes/atlantis-print/brand.ts
+++ src/themes/atlantis-print/brand.ts
@@ -1,1 +1,1 @@
-export const BRAND_PRIMARY = '#18A7D0';
+export const BRAND = { bg: '#F7F8FC', line: '#E3E8F0', ink: '#161A22', muted: '#667487', primary: '#18A7D0', primaryDark: '#127B98', accent: '#7B3FE4', black: '#0F1012' };

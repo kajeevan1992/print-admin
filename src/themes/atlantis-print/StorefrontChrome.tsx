6251bcbe7d6b0ffa078a9250921565a37527492c
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-30 13:57:18 +0100

    Add modular Atlantis footer to chrome

--- src/themes/atlantis-print/StorefrontChrome.tsx
+++ src/themes/atlantis-print/StorefrontChrome.tsx
@@ -1,3 +1,4 @@
 import AtlantisHeader from './AtlantisHeader';
+import AtlantisFooter from './AtlantisFooter';
 
-export default function StorefrontChrome({currentPath='/', navItems=[], storeBase='/', children}: any){return <div><AtlantisHeader currentPath={currentPath} navItems={navItems} storeBase={storeBase} />{children}</div>;}
+export default function StorefrontChrome({currentPath='/', navItems=[], storeBase='/', children}: any){return <div><AtlantisHeader currentPath={currentPath} navItems={navItems} storeBase={storeBase} />{children}<AtlantisFooter storeBase={storeBase} /></div>;}

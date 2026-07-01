4d7de6b57d47aad0ed6f2dd2db22d88f6de6ae2d
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-01 09:19:23 +0100

    Add Atlantis storefront runtime renderer

---
+++ src/theme-runtime/atlantis-renderer.tsx
@@ -0,0 +1,14 @@
+import EnhancedHomePage from '@/themes/atlantis-native/EnhancedHomePage';
+import CategoryPage from '@/themes/atlantis-native/CategoryPage';
+import ProductPage from '@/themes/atlantis-native/ProductPage';
+import CollectionPointsPage from '@/themes/atlantis-native/CollectionPointsPage';
+import { loadCollectionPoints } from '@/themes/atlantis-native/collection-points';
+import type { StorefrontRuntimeContext } from './types';
+
+export async function renderAtlantisStorefront(context: StorefrontRuntimeContext) {
+  const { storeBase, navItems, products, routeSegments, tenantIds } = context;
+  if (!routeSegments.length) return <EnhancedHomePage storeBase={storeBase} navItems={navItems} products={products} />;
+  if (routeSegments[0] === 'collection-points') return <CollectionPointsPage storeBase={storeBase} navItems={navItems} points={await loadCollectionPoints(tenantIds)} />;
+  if (routeSegments.length >= 2) return <ProductPage storeBase={storeBase} navItems={navItems} category={routeSegments[0]} slug={routeSegments[routeSegments.length - 1]} products={products} />;
+  return <CategoryPage storeBase={storeBase} navItems={navItems} slug={routeSegments[0]} products={products} />;
+}

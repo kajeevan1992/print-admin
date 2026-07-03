7d11e130dd460327c8940344a98fc8b8fe608edd
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-03 21:26:44 +0100

    Route native cart page

--- src/theme-runtime/atlantis-renderer.tsx
+++ src/theme-runtime/atlantis-renderer.tsx
@@ -3,6 +3,7 @@ import CategoryPage from '@/themes/atlantis-native/CategoryPage';
 import ProductPage from '@/themes/atlantis-native/ProductPage';
 import QuoteRequestPage from '@/themes/atlantis-native/QuoteRequestPage';
 import CollectionPointsPage from '@/themes/atlantis-native/CollectionPointsPage';
+import CartPage from '@/themes/atlantis-native/CartPage';
 import { loadCollectionPoints } from '@/themes/atlantis-native/collection-points';
 import type { StorefrontRuntimeContext } from './types';
 
@@ -10,6 +11,7 @@ export async function renderAtlantisStorefront(context: StorefrontRuntimeContext
   const { tenantSlug, storeSlug, storeBase, navItems, products, routeSegments, tenantIds, collectionPoints } = context;
   if (!routeSegments.length) return <EnhancedHomePage storeBase={storeBase} navItems={navItems} products={products} />;
   if (routeSegments[0] === 'collection-points') return <CollectionPointsPage storeBase={storeBase} navItems={navItems} points={collectionPoints || await loadCollectionPoints(tenantIds)} />;
+  if (routeSegments[0] === 'cart') return <CartPage storeBase={storeBase} navItems={navItems} products={products} />;
   if (routeSegments[0] === 'quote' && routeSegments.length >= 3) return <QuoteRequestPage storeBase={storeBase} navItems={navItems} tenantSlug={tenantSlug} storeSlug={storeSlug} category={routeSegments[1]} slug={routeSegments[2]} products={products} />;
   if (routeSegments.length >= 2) return <ProductPage storeBase={storeBase} navItems={navItems} category={routeSegments[0]} slug={routeSegments[routeSegments.length - 1]} products={products} />;
   return <CategoryPage storeBase={storeBase} navItems={navItems} slug={routeSegments[0]} products={products} />;

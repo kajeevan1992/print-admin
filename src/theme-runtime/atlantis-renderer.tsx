d2ad3aa34f67902efb72ea8a5b89c0f25abd0691
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-06 17:56:09 +0100

    Pass native tenant context to product page

--- src/theme-runtime/atlantis-renderer.tsx
+++ src/theme-runtime/atlantis-renderer.tsx
@@ -16,6 +16,6 @@ export async function renderAtlantisStorefront(context: StorefrontRuntimeContext
   if (routeSegments[0] === 'checkout-cancel') return <CheckoutStatusPage storeBase={storeBase} navItems={navItems} status="cancel" searchParams={searchParams || {}} />;
   if (routeSegments[0] === 'cart') return <CartPage tenantSlug={tenantSlug} storeSlug={storeSlug} storeBase={storeBase} navItems={navItems} productSlug={searchParams?.product} categorySlug={searchParams?.category} products={products} searchParams={searchParams || {}} />;
   if (routeSegments[0] === 'quote' && routeSegments.length >= 3) return <QuoteRequestPage storeBase={storeBase} navItems={navItems} tenantSlug={tenantSlug} storeSlug={storeSlug} category={routeSegments[1]} slug={routeSegments[2]} products={products} searchParams={searchParams || {}} />;
-  if (routeSegments.length >= 2) return <ProductPage storeBase={storeBase} navItems={navItems} category={routeSegments[0]} slug={routeSegments[routeSegments.length - 1]} products={products} searchParams={searchParams || {}} />;
+  if (routeSegments.length >= 2) return <ProductPage tenantSlug={tenantSlug} storeSlug={storeSlug} storeBase={storeBase} navItems={navItems} category={routeSegments[0]} slug={routeSegments[routeSegments.length - 1]} products={products} searchParams={searchParams || {}} />;
   return <CategoryPage storeBase={storeBase} navItems={navItems} slug={routeSegments[0]} products={products} />;
 }

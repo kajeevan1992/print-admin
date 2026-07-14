cdb9bb95220d16a2e2e3a71a1c35a3d11e066593
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-14 23:53:03 +0100

    Add launch command centre route

---
+++ app/launch-command-centre/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchCommandCentrePage } from '@/modules/launch/pages/launch-command-centre-page';
+
+export default function Page() {
+  return <LaunchCommandCentrePage />;
+}

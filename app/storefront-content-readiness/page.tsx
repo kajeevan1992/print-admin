bd08ef9a41106fed84cc5245e4ae7f81885492b1
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-14 22:57:15 +0100

    Add storefront content readiness route

---
+++ app/storefront-content-readiness/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { StorefrontContentReadinessPage } from '@/modules/launch/pages/storefront-content-readiness-page';
+
+export default function Page() {
+  return <StorefrontContentReadinessPage />;
+}

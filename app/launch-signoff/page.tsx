234c2ef18abccb90e4349a60ce69f087ee3a9a3a
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-14 23:24:01 +0100

    Add signoff route

---
+++ app/launch-signoff/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { LaunchSignOffPage } from '@/modules/launch/pages/launch-sign-off-page';
+
+export default function Page() {
+  return <LaunchSignOffPage />;
+}

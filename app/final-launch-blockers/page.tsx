52c7279c37809faed0259f14d325bd9da7fb9fed
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-14 17:56:14 +0100

    Add final launch blockers route

---
+++ app/final-launch-blockers/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { FinalLaunchBlockersPage } from '@/modules/launch/pages/final-launch-blockers-page';
+
+export default function Page() {
+  return <FinalLaunchBlockersPage />;
+}

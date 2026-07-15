34604f6876b198d63e9fedbba076dc8ac81f7f7f
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-15 11:57:50 +0100

    Add post launch health route

---
+++ app/post-launch-health/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PostLaunchHealthPage } from '@/modules/launch/pages/post-launch-health-page';
+
+export default function Page() {
+  return <PostLaunchHealthPage />;
+}

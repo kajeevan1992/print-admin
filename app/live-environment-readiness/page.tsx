0b1b0816d29d915a7f74cd55f0db6f9a706f2897
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-15 14:23:03 +0100

    Add live environment readiness route

---
+++ app/live-environment-readiness/page.tsx
@@ -0,0 +1,7 @@
+import { LiveEnvironmentReadinessPage } from '@/modules/launch/pages/live-environment-readiness-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <LiveEnvironmentReadinessPage />;
+}

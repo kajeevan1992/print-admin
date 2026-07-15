fa55789a6ff5820152104216f3b209ec73fe7d17
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-15 17:17:02 +0100

    Add public endpoint abuse readiness route

---
+++ app/public-endpoint-abuse-readiness/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { PublicEndpointAbuseReadinessPage } from '@/modules/launch/pages/public-endpoint-abuse-readiness-page';
+
+export default function Page() {
+  return <PublicEndpointAbuseReadinessPage />;
+}

e5c9075a580eeea2c4fd1bed88797adae1c24a5b
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-16 21:32:25 +0100

    Add standard Canvas package exports

---
+++ src/v0-themes/canvas/index.ts
@@ -0,0 +1,3 @@
+export { default as ThemeHomePage } from './CanvasHomePage';
+export { CANVAS_ROUTE_VIEWS as themeRouteViews } from './RouteViews';
+export { CANVAS_V0_MANIFEST as themeManifest } from './manifest';

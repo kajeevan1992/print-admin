ae6b9a00c761242926959dfdb4a5701d9f813968
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-17 09:30:55 +0100

    Add advanced fulfilment rules route

---
+++ app/fulfilment-rules/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import { FulfilmentRulesPage } from '@/modules/operations/pages/fulfilment-rules-page';
+
+export default function Page() {
+  return <FulfilmentRulesPage />;
+}

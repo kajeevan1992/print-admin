a6e5191a9bf9502cf89999405e5281b077ab5ac7
kajeevan1992 <kajeevan1992@icloud.com>
2026-07-22 21:32:31 +0100

    Export HOLO V2 theme package

---
+++ src/v0-themes/holo-v2/index.ts
@@ -0,0 +1,3 @@
+export { default as ThemeHomePage } from './ThemeHomePage';
+export { HOLOV2_ROUTE_VIEWS as themeRouteViews } from './RouteViews';
+export { HOLOV2_V0_MANIFEST as themeManifest } from './manifest';
